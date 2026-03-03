3e2963c5e8eb31a8613363b8b89b7cd0afdcd0e0
João Victor Ramiro <85127729+Jvramiro@users.noreply.github.com>
2026-03-03 17:06:56 -0300

    Setup SqLite Connection String

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WorkOutAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
