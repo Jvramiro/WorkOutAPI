ae39d103fafcebf933c616d2ee9693f3fab2e444
João Victor Ramiro <85127729+Jvramiro@users.noreply.github.com>
2026-03-03 17:13:23 -0300

    Update Adding Swagger to Development Environment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlite(
         builder.Configuration.GetConnectionString("DefaultConnection")
@@ -12,6 +15,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 );
 
 var app = builder.Build();
+
+if(app.Environment.IsDevelopment()){
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
